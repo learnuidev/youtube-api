34a57cc25665266fbfb437a59568b1b75018dd8d
learnuidev <learnuidev@gmail.com>
2024-12-30 12:37:15 -0500

    get video by id

---
+++ 02_get_video_by_id.js
@@ -0,0 +1,27 @@
+const { youtube } = require("./youtube");
+
+async function getVideoById(videoId) {
+  try {
+    const response = await youtube.videos.list({
+      part: "snippet,contentDetails,statistics",
+      id: videoId,
+    });
+
+    if (response.data.items.length > 0) {
+      const video = response.data.items[0];
+      //   console.log(`Title: ${video.snippet.title}`);
+      //   console.log(`Description: ${video.snippet.description}`);
+      //   console.log(`Views: ${video.statistics.viewCount}`);
+
+      return video;
+    } else {
+      console.log("No video found with that ID.");
+    }
+  } catch (error) {
+    console.error("Error fetching video:", error);
+  }
+}
+
+getVideoById("2JJaCwMkC00").then((video) => {
+  console.log("VIDEO", video);
+});
